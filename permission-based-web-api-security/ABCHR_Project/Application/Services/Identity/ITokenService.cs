ab6125f2d3d16d022dadac9ea8e02efefc01ec73
Atanas (Tony) Hristov <atanas.y.hristov@gmail.com>
2024-03-02 12:47:25 -0600

    Section 7: Application Models (request, response) and Interfaces

---
+++ permission-based-web-api-security/ABCHR_Project/Application/Services/Identity/ITokenService.cs
@@ -0,0 +1,12 @@
+﻿using Common.Requests.Identity;
+using Common.Responses.Identity;
+using Common.Responses.Wrappers;
+
+namespace Application.Services.Identity
+{
+    public interface ITokenService
+    {
+        Task<ResponseWrapper<TokenResponse>> GetTokenAsync(TokenRequest tokenRequest);
+        Task<ResponseWrapper<TokenResponse>> GetRefreshTokenAsync(RefreshTokenRequest refreshTokenRequest);
+    }
+}
